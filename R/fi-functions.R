13422f12db3b511775f87bd341be1f4210a0415d
Michael Lydeamore <games.freak41@gmail.com>
2018-05-31 09:38:05 +1000

    Change Fisher Information function to a more sensible name

--- R/fi-functions.R
+++ R/fi-functions.R
@@ -58,7 +58,7 @@
   (gamma^2*exp(2*gamma*t + 2*lamb*t) - gamma^2 - 2*gamma*lamb + ((gamma^3*lamb + 2*gamma^2*lamb^2 + gamma*lamb^3)*t^2 + 2*gamma*lamb - 2*(gamma^3 + 2*gamma^2*lamb + gamma*lamb^2)*t)*exp(gamma*t + lamb*t))/(gamma^2*lamb^2 + 2*gamma*lamb^3 + lamb^4 + (gamma^4 + 2*gamma^3*lamb + gamma^2*lamb^2)*exp(2*gamma*t + 2*lamb*t) + 2*(gamma^3*lamb + 2*gamma^2*lamb^2 + gamma*lamb^3)*exp(gamma*t + lamb*t))
 }
 
-FI <- function(gamma, lambda, delta, n_obs, i0=0.316)
+calculateFisherInformation <- function(gamma, lambda, delta, n_obs, i0=0.316)
 {
   P <- matrix(c(gamma+lambda*exp(-delta*(gamma+lambda)), lambda-lambda*exp(-delta*(gamma+lambda)), gamma-gamma*exp(-delta*(gamma+lambda)), lambda+gamma*exp(-delta*(gamma+lambda)))/(gamma+lambda), byrow = T, nrow=2)
   
@@ -74,16 +74,16 @@ FI <- function(gamma, lambda, delta, n_obs, i0=0.316)
   
   for (i in 1:n_obs)
   {
-    top_left <- top_left + sum((x_0 %*% (P%^%i))[,1])*( (1-phi_s)*.one_minus_phi_s_lambda_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_lambda(gamma,lambda,delta) ) +
+    top_left <- top_left + sum((x_0 %*% (P%^%(i-1)))[,1])*( (1-phi_s)*.one_minus_phi_s_lambda_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_lambda(gamma,lambda,delta) ) +
       sum((x_0%*% (P%^%i))[,2])*( (1-phi_i)*.one_minus_phi_i_lambda_lambda(gamma,lambda,delta) + phi_i*.phi_i_lambda_lambda(gamma,lambda,delta) )
     
-    top_right <- top_right + sum((x_0 %*% (P%^%i))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_gamma(gamma,lambda,delta) ) +
+    top_right <- top_right + sum((x_0 %*% (P%^%(i-1)))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_gamma(gamma,lambda,delta) ) +
       sum((x_0%*% (P%^%i))[,2])*( (1-phi_i)*.one_minus_phi_i_lambda_gamma(gamma,lambda,delta) + phi_i*.phi_i_lambda_gamma(gamma,lambda,delta) )
 
-    bottom_left <- bottom_left + sum((x_0 %*% (P%^%i))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_gamma(gamma,lambda,delta) ) +
+    bottom_left <- bottom_left + sum((x_0 %*% (P%^%(i-1)))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_lambda(gamma,lambda,delta) + phi_s*.phi_s_lambda_gamma(gamma,lambda,delta) ) +
       sum((x_0%*% (P%^%i))[,2])*( (1-phi_i)*.one_minus_phi_i_lambda_gamma(gamma,lambda,delta) + phi_i*.phi_i_lambda_gamma(gamma,lambda,delta) )
 
-    bottom_right <- bottom_right + sum((x_0 %*% (P%^%i))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_gamma(gamma,lambda,delta) + phi_s*.phi_s_gamma_gamma(gamma,lambda,delta) ) +
+    bottom_right <- bottom_right + sum((x_0 %*% (P%^%(i-1)))[,1])*( (1-phi_s)*.one_minus_phi_s_gamma_gamma(gamma,lambda,delta) + phi_s*.phi_s_gamma_gamma(gamma,lambda,delta) ) +
       sum((x_0%*% (P%^%i))[,2])*( (1-phi_i)*.one_minus_phi_i_gamma_gamma(gamma,lambda,delta) + phi_i*.phi_i_gamma_gamma(gamma,lambda,delta) )
   }
   
